6059ef511e7b0bb5291b1e6627f3628d753157e8
sridhar rao <sridhar.tr22@gmail.com>
2020-04-02 10:45:28 +0530

    created etracker-app project structure using spring initializr and added dependencies - Spring Web,Spring Jpa, MySql Connector.

---
+++ etracker-app/src/main/java/com/hackathon/etracker/EtrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.etracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtrackerAppApplication.class, args);
+	}
+
+}
